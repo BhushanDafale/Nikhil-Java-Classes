069d7f7fdd70ac3d9563dfc006984be6520f46c2
BhushanDafale <bhushan.dafale7@gmail.com>
2020-07-20 19:23:49 +0530

    static tutorial part 1

---
+++ src/tutorial/java/staticdemo/MainApp.java
@@ -0,0 +1,44 @@
+package tutorial.java.staticdemo;
+
+public class MainApp {
+	
+	// specifier type variable-name = value
+	static int a = 10;
+	int b = 20;
+	
+	public static void main(String[] args) {
+		
+		MainApp ma1 = new MainApp();
+		ma1.b++; // b = b + 1;
+		System.out.println("ma1: b - " + ma1.b);
+		
+		MainApp ma2 = new MainApp();
+		ma2.b++;
+		ma2.b++;
+		ma2.b++;
+		System.out.println("ma1: b - " + ma2.b);
+		
+		System.out.println("before: a - " + a);
+		System.out.println("ma1: a - " + ma1.a);
+		System.out.println("ma2: a - " + ma2.a);
+		a++;
+		System.out.println("after a - " + a);
+		increment();
+		System.out.println("ma1: a - " + ma1.a);
+		System.out.println("ma2: a - " + ma2.a);
+		
+//		StaticHelper sh = new StaticHelper();
+//		System.out.println(sh.a);
+//		
+//		System.out.println(sh.b);
+//		System.out.println(StaticHelper.b);
+//		
+//		StaticHelper.display();
+		
+	}
+	
+	public static void increment() {
+		a++;
+	}
+
+}
---
+++ src/tutorial/java/staticdemo/StaticHelper.java
@@ -0,0 +1,17 @@
+package tutorial.java.staticdemo;
+
+public class StaticHelper {
+	
+	int a = 30;
+	
+	static int b = 50;
+	
+	public static void display() {
+		System.out.println("-- display");
+	}
+	
+	public void print() {
+		System.out.println("-- print");
+	}
+
+}
