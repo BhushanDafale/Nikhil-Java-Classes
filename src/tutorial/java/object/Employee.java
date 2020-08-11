6ef443003e60ac8cfa0ccc7809a95a0fff749569
BhushanDafale <bhushan.dafale7@gmail.com>
2020-08-11 22:07:25 +0530

    object class - part 1

---
+++ src/tutorial/java/object/Employee.java
@@ -0,0 +1,28 @@
+package tutorial.java.object;
+
+public class Employee {
+
+	private String name;
+	private Double salary;
+
+	public Employee() {
+		
+	}
+	
+	public Employee(String name, Double salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Employee [name=" + name + ", salary=" + salary + "]";
+//	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+	}
+
+}
