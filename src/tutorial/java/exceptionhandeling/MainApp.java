5f291fbe0abc6743a03e43589680afd2b268f109
BhushanDafale <bhushan.dafale7@gmail.com>
2020-08-13 20:06:45 +0530

    exception handeling - day 2

--- src/tutorial/java/exceptionhandeling/MainApp.java
+++ src/tutorial/java/exceptionhandeling/MainApp.java
@@ -2,30 +2,40 @@ package tutorial.java.exceptionhandeling;
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		
-		String str = "Bhushan";	
-		System.out.println("str - " + str);
-		
-		try {
-			System.out.println("--inside try");
-			
-			str.toLowerCase();
-			
-			System.out.println("--try end");
-			
-			int a = 10/0;
-			
-		} catch(ArithmeticException e) {
-			System.out.println("AE-- exception occured: " + e.toString());
-		} catch(NullPointerException e) {
-			System.out.println("NP-- exception occured: " + e.toString());
-		} finally {
-			System.out.println("-- finally block");
-		} 
-		
-		System.out.println("---- end of program");
-		
+	public static void method_A() {
+		System.out.println("--method_A start");
+		method_B();
+		System.out.println("--method_A end");
 	}
 
+	public static void method_D() {
+		System.out.println("--method_D end");
+	}
+
+	public static void method_C() {
+		System.out.println("--method_C start");
+
+		int a = 10 / 0; // AE
+		System.out.println("---after exception");
+		method_D();
+		System.out.println("--method_C end");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("--main start");
+
+		try {
+			method_A();
+		} catch (Exception e) {
+			System.out.println("-AE----");
+		}
+
+		System.out.println("--main end");
+	}
+
+	public static void method_B() {
+		System.out.println("--method_B start");
+		method_C();
+		System.out.println("--method_B end");
+	}
 }
